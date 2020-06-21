cc841181ca201c4cf90767487101ba86e25c3053
Chloe Kim <chloeminkyung@gmail.com>
2020-06-21 16:51:38 +0900

    updated index, about page with additional info and added new image for car turkey

--- js/main.js
+++ js/main.js
@@ -558,6 +558,62 @@
     });
   }
 
+  function initApplicationForm() {
+
+    $('.application-form').on('submit', function(event) {
+
+      event.preventDefault();
+
+      var requiredInputs = $(this).find('input[data-required="true"], textarea[data-required="true"]').toArray();
+
+      var isValidForm = function() {
+        var toReturn;
+
+        requiredInputs.forEach(function(element, index){
+          if (!$(element).val()) {
+            toReturn = false;
+          } else{
+            toReturn = true;
+          }
+        });
+
+        return toReturn;
+      }
+
+      requiredInputs.forEach(function(element, index){
+        if (!$(element).val()) {
+          $(element).parent('.form-group').addClass('has-error');
+        } else{
+          $(element).parent('.form-group').removeClass('has-error');
+        }
+      });
+
+      if (isValidForm()) {
+        $.ajax({
+          url: $(this).attr('action'),
+          type: 'POST',
+          data: $(this).serialize(),
+          crossDomain: true,
+          xhrFields: {
+            withCredentials: true
+          }
+        })
+        var message = $(this).data('success-text') || 'Thanks for your application!<br>Your application has been sent. We will get back to you shortly!';
+        var succesTemplate = '<div role="alert" class="alert alert-success alert-dismissible alert-colored text-center">'+ message +'</div>';
+        $(this).find('[role="alert"]').fadeOut(300);
+        $(succesTemplate).insertBefore($(this).find('button'));
+        $(this).find('button')[0].disabled = true;
+      }
+    });
+
+    $('.application-form input, .application-form textarea').on('keyup', function(event) {
+      event.preventDefault();
+      if ($(this).val()) {
+        $(this).parent('.form-group').removeClass('has-error');
+      }
+    });
+  }
+
   function initCounters () {
     
     $('.counter').appear(function() {
@@ -745,6 +801,7 @@
     initForms();
     initGeneral();
     initCustom();
+    initApplicationForm();
 
     if ($('#map').length) {
       google.maps.event.addDomListener(window, 'load', initMap);
